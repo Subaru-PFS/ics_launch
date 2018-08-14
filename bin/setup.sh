919a9b5dd614ce2259675287e0680f93ecd47c45
Craig Loomis <cloomis@astro.princeton.edu>
2018-08-14 09:05:16 -0400

    Initial files

---
+++ bin/setup.sh
@@ -0,0 +1,6 @@
+PATH=/software/conda/bin:$PATH
+
+# Use "default/" EUPS and not a specific version.
+# Use the MHS eups by default, not the DRP version.
+#
+source /software/mhs/products/eups/default/bin/setups.sh
