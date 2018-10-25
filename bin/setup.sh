3c00f4672910e993da9c24faef6e2a8fdbf3f5cc
Craig Loomis <cloomis@astro.princeton.edu>
2018-10-25 01:09:33 -0400

    Add /software/local/bin

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,6 +1,12 @@
-PATH=/software/conda/bin:$PATH
+# Use the project conda/python, and a shared ../local/bin
+#
+PATH=/software/conda/bin:/software/local/bin:$PATH
 
 # Use "default/" EUPS and not a specific version.
 # Use the MHS eups by default, not the DRP version.
 #
 source /software/mhs/products/eups/default/bin/setups.sh
+
+# Get the basics (oneCmd.py, mainly)
+#
+setup tron_actorcore
