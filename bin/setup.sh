1cd0c6510fa22729dc0d6e81944ca062fbad2dcd
Craig Loomis <cloomis@astro.princeton.edu>
2018-12-19 14:33:38 -0500

    Make POSIX-compliant

--- bin/setup.sh
+++ bin/setup.sh
@@ -5,7 +5,7 @@ PATH=/software/conda/bin:/software/local/bin:$PATH
 # Use "default/" EUPS and not a specific version.
 # Use the MHS eups by default, not the DRP version.
 #
-source /software/mhs/products/eups/default/bin/setups.sh
+. /software/mhs/products/eups/default/bin/setups.sh
 
 # Get the basics (oneCmd.py, mainly)
 #
