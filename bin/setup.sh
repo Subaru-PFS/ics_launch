03cbffbd5c0d5eb00f395d106395ca4cc39a6e28
Craig Loomis <cloomis@astro.princeton.edu>
2019-03-04 08:25:30 -0500

    Add our own bin/ to $PATH

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,11 +1,12 @@
 # Use the project conda/python, and a shared ../local/bin
 #
-PATH=/software/conda/bin:/software/local/bin:$PATH
+PATH=/software/conda/bin:/software/local/bin:/software/pfs_launch/bin:$PATH
 
 # Use "default/" EUPS and not a specific version.
 # Use the MHS eups by default, not the DRP version.
 #
 . /software/mhs/products/eups/default/bin/setups.sh
+declare -fx setup unsetup
 
 # Get the basics (oneCmd.py, mainly)
 #
