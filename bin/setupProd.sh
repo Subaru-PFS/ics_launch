689c6eca2237d4bbae0794372378b44393eaa085
Craig Loomis <cloomis@astro.princeton.edu>
2019-03-04 08:26:13 -0500

    Allow overriding individual products
    version.txt is now:
       hostname actorname overrideProduct version
    
    This requires specifying more stuff, but allows overriding individual
    products (e.g. tron_actorcore).

---
+++ bin/setupProd.sh
@@ -0,0 +1,24 @@
+OIFS=$IFS
+trap 'IFS=$OIFS' EXIT
+
+actor=$1; shift
+
+# Use getopts and make hostname require -h
+hostname=$1; shift
+args="$@"
+
+if test -z "$actor"; then
+    echo "usage: $0 actorName hostName" 1>&2
+    return 1
+fi
+
+# Make life a bit easier by always setting up the current version first.
+#
+setup $args $actor
+
+# Error checking is non-existant. Bad, Loomis.
+#
+IFS=$(echo)
+for versionLine in $(egrep "^$hostname *$actor" /software/pfs_launch/versions.txt); do
+    eval $(echo $versionLine | cut -d' ' -f1-2 --complement | awk "{print \"setup $args \" \$0}")
+done
