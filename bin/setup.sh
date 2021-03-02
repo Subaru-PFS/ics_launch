79f2006721dd921b5128e39f870cf8b41e3af614
Craig Loomis <cloomis@astro.princeton.edu>
2021-03-02 13:45:26 -1000

    Use new-style conda initialization.
    Allows `conda activate $env` to work.

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,6 +1,26 @@
+# Maybe not the right place or necessary, but averts some problems.
+umask 2
+
 # Use the project conda/python, and a shared ../local/bin
 #
-PATH=/software/conda/bin:/software/local/bin:$PATH
+PATH=/software/bin:/software/local/bin:$PATH
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/software/conda/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/software/conda/etc/profile.d/conda.sh" ]; then
+        . "/software/conda/etc/profile.d/conda.sh"
+    else
+        export PATH="/software/conda/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+declare -fx conda
+# <<< conda initialize <<<
+
 # This is the environment for the ICS software.
 conda activate conda-ics
 
